f38db1a3b1ec2a954042a1e21e16a4724c670d0c
Fabrizio Romanelli <fabrizio.romanelli@gmail.com>
2020-04-28 17:18:53 +0200

    First commit

---
+++ src/example2/src/listener.cpp
@@ -0,0 +1,20 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+void talkerCallback(const std_msgs::String::ConstPtr& msg)
+{
+  ROS_INFO("Ho sentito: [%s]", msg->data.c_str());
+}
+
+int main(int argc, char **argv)
+{
+  // Inizializza ROS con un nodo denominato "listener"
+  ros::init(argc, argv, "listener");
+
+  ros::NodeHandle n;
+  ros::Subscriber listener_sub = n.subscribe("talkTopic", 1000, talkerCallback);
+
+  ros::spin();
+
+  return 0;
+}
