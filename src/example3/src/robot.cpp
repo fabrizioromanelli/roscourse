55a404daf7fc4b29a153274d781769af114b0aef
Fabrizio Romanelli <fabrizio.romanelli@gmail.com>
2020-04-29 12:35:13 +0200

    Adding formatting for laser scans

--- src/example3/src/robot.cpp
+++ src/example3/src/robot.cpp
@@ -1,18 +1,21 @@
 #include "ros/ros.h"
 #include "example3/laser.h"
 
+using namespace std;
+
 void sensorCallback(const example3::laser::ConstPtr& msg)
 {
   ROS_INFO("Msg timestamp   : [%u.%u]", msg->header.stamp.sec, msg->header.stamp.nsec);
-  ROS_INFO("Start scan angle: %2.2f[rad] | End scan angle: %2.2f[rad] | Angle increment: %2.2f[rad]", msg->angle_min, msg->angle_max, msg->angle_increment);
-  ROS_INFO("Minimum range   : %2.2f[m] | Maximum range: %2.2f[m]", msg->range_min, msg->range_max);
-  int l = 0;
+  ROS_INFO("Start scan angle: %2.2f(rad) | End scan angle: %2.2f(rad) | Angle increment: %2.2f(rad)", msg->angle_min, msg->angle_max, msg->angle_increment);
+  ROS_INFO("Minimum range   : %2.2f(m) | Maximum range: %2.2f(m)", msg->range_min, msg->range_max);
+
   for (auto it = msg->ranges.begin(); it != msg->ranges.end(); ++it)
   {
-    std::cout << "[" << l << "]: " << *it << std::endl;
-    l++;
+    if ( ((it - msg->ranges.begin()) % 20) == 0 && (it - msg->ranges.begin()) != 0 )
+      cout << endl;
+    cout << fixed << setw(4) << setprecision(2) << *it << " ";
   }
-
+  cout << flush << endl;
 }
 
 int main(int argc, char **argv)
